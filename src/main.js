300b1e43fb4f617150dbba141944ef64c8dd253a
DmitriySokolov2023 <sokolovoff2023@gmail.com>
2025-04-03 11:47:05 +0300

    add-base-tempaltes

--- src/main.js
+++ src/main.js
@@ -1,23 +1,29 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
+import './style.css'
 
 document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
+  <div class='container'>
+   <header><p>Шапка</p></header>
+   <main>
+    <div class="tasks">
+    <div class="task">
+    <div class="task__text">Задание</div>
+    <div class="task__status">Статус</div>
     </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
+    <div class="task">
+    <div class="task__text"><a href="/">Text</a></div>
+    <div class="task__status">X</div>
+    </div>
+    <div class="task"><div class="task__text"><a href="/">Text</a></div>
+    <div class="task__status">X</div></div>
+    <div class="task"><div class="task__text"><a href="/">Text</a></div>
+    <div class="task__status">X</div></div>
+    <div class="task"><div class="task__text"><a href="/">Text</a></div>
+    <div class="task__status">X</div></div>
+    <div class="task"><div class="task__text"><a href="/">Text</a></div>
+    <div class="task__status">X</div></div>
+    </div>
+   </main>
   </div>
 `
 
